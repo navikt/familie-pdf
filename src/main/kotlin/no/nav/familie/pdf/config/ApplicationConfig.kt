9bd004c67b6494ca7faec700d6d5b40d1682918b
Viktor Solberg <viktorgsolberg@gmail.com>
2024-11-10 17:32:23 +0100

    fjernet unødvendige config imports

--- src/main/kotlin/no/nav/familie/pdf/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/familie/pdf/config/ApplicationConfig.kt
@@ -1,15 +1,8 @@
 package no.nav.familie.pdf.config
 
 import com.fasterxml.jackson.module.kotlin.KotlinModule
-import no.nav.familie.http.config.RestTemplateBuilderBean
-import no.nav.familie.http.interceptor.BearerTokenClientInterceptor
-import no.nav.familie.http.interceptor.BearerTokenExchangeClientInterceptor
-import no.nav.familie.http.interceptor.ConsumerIdClientInterceptor
-import no.nav.familie.http.interceptor.MdcValuesPropagatingClientInterceptor
-import no.nav.familie.kafka.KafkaErrorHandler
 import no.nav.familie.log.filter.LogFilter
 import no.nav.familie.log.filter.RequestTimeFilter
-import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringBootConfiguration
@@ -17,23 +10,11 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Import
-import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootConfiguration
 @ComponentScan("no.nav.familie.pdf")
 @ConfigurationPropertiesScan
-@EnableOAuth2Client(cacheEnabled = true)
 @EnableJwtTokenValidation(ignore = ["org.springframework", "org.springdoc"])
-@EnableScheduling
-@Import(
-    BearerTokenClientInterceptor::class,
-    BearerTokenExchangeClientInterceptor::class,
-    RestTemplateBuilderBean::class,
-    MdcValuesPropagatingClientInterceptor::class,
-    ConsumerIdClientInterceptor::class,
-    KafkaErrorHandler::class,
-)
 class ApplicationConfig {
     private val logger = LoggerFactory.getLogger(ApplicationConfig::class.java)
 
