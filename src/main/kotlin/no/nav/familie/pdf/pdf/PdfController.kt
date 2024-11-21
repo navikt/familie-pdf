d70555c83ed59e9f12c785d2d1b8de040f2cb157
Sandra Lekve <sandra.lekve@icloud.com>
2024-11-21 14:16:07 +0100

    flytte test endepunkt til test controller

--- src/main/kotlin/no/nav/familie/pdf/pdf/PdfController.kt
+++ src/main/kotlin/no/nav/familie/pdf/pdf/PdfController.kt
@@ -1,8 +1,6 @@
 package no.nav.familie.pdf.pdf
 
-import no.nav.familie.pdf.pdf.domain.PdfMedStandarder
 import no.nav.security.token.support.core.api.Unprotected
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,10 +16,4 @@ class PdfController {
     fun opprettPdf(
         @RequestBody søknad: Map<String, Any>,
     ): ByteArray = pdfOppretterService.opprettPdf(søknad)
-
-    @CrossOrigin(origins = ["http://localhost:5173"])
-    @PostMapping("/opprett-pdf-med-standarder")
-    fun opprettPdfMedValidering(
-        @RequestBody søknad: Map<String, Any>,
-    ): PdfMedStandarder = pdfOppretterService.opprettPdfMedStandarder(søknad)
 }
--- src/main/kotlin/no/nav/familie/pdf/pdf/TestPdfController.kt
+++ src/main/kotlin/no/nav/familie/pdf/pdf/TestPdfController.kt
@@ -4,6 +4,8 @@ import no.nav.familie.pdf.pdf.domain.PdfMedStandarder
 import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,8 +14,15 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("api/test-pdf")
 class TestPdfController {
     private val testPdfService = TestPdfService()
+    private val pdfService = PdfService()
 
     @CrossOrigin(origins = ["http://localhost:5173"])
-    @GetMapping("/hent-pdf-med-standarder")
+    @GetMapping("/pdf-med-standarder")
     fun hentPdfFraResourceMedStandarder(): PdfMedStandarder = testPdfService.opprettTestPdfMedStandarder()
+
+    @CrossOrigin(origins = ["http://localhost:5173"])
+    @PostMapping("/pdf-med-standarder")
+    fun opprettPdfMedValidering(
+        @RequestBody søknad: Map<String, Any>,
+    ): PdfMedStandarder = pdfService.opprettPdfMedStandarder(søknad)
 }
