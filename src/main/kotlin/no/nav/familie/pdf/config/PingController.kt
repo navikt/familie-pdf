b29decb43f87d473215e655d9b263faa2b47ac11
Viktor Solberg <viktorgsolberg@gmail.com>
2024-11-22 14:34:47 +0100

    skal legge til ping-controller (#9)

---
+++ src/main/kotlin/no/nav/familie/pdf/config/PingController.kt
@@ -0,0 +1,15 @@
+package no.nav.familie.pdf.config
+
+import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@Unprotected
+@RestController
+@RequestMapping("/api/ping")
+class PingController {
+    @GetMapping
+    fun ping(): ResponseEntity<String> = ResponseEntity.ok("pong")
+}
