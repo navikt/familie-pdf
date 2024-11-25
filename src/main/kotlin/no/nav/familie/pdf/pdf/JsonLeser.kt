7628c4c969aadccd67999fea775c06f419a25e0b
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2024-11-25 08:59:30 +0100

    Tok tilbake jsonleseren

---
+++ src/main/kotlin/no/nav/familie/pdf/pdf/JsonLeser.kt
@@ -0,0 +1,29 @@
+package no.nav.familie.pdf.pdf
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import java.io.FileNotFoundException
+import java.io.IOException
+
+object JsonLeser {
+    private val objectMapper: ObjectMapper =
+        jacksonObjectMapper()
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            .registerModule(JavaTimeModule())
+
+    fun lesSøknadJson(): Map<String, Any> {
+        val jsonInputStream =
+            this::class.java.getResourceAsStream("/søknad.json")
+                ?: throw FileNotFoundException("Kan ikke lese søknad.json")
+
+        return try {
+            jsonInputStream.bufferedReader().use { reader ->
+                objectMapper.readValue(reader, Map::class.java) as Map<String, Any>
+            }
+        } catch (e: IOException) {
+            throw RuntimeException("Feil ved lesing av JSON-fil", e)
+        }
+    }
+}
--- src/main/kotlin/no/nav/familie/pdf/pdf/TestPdfService.kt
+++ src/main/kotlin/no/nav/familie/pdf/pdf/TestPdfService.kt
@@ -1,21 +1,12 @@
 package no.nav.familie.pdf.pdf
 
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import no.nav.familie.pdf.pdf.JsonLeser.lesSøknadJson
 import no.nav.familie.pdf.pdf.domain.PdfMedStandarder
 import no.nav.familie.pdf.pdf.domain.PdfStandard
 import no.nav.familie.pdf.pdf.domain.Standard
-import java.io.FileNotFoundException
-import java.io.IOException
 
 class TestPdfService(
     private val pdfService: PdfService,
-    private val objectMapper: ObjectMapper =
-        jacksonObjectMapper()
-            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-            .registerModule(JavaTimeModule()),
 ) {
     fun opprettTestPdfMedStandarder(): PdfMedStandarder {
         val feltMap = lesSøknadJson()
@@ -38,19 +29,5 @@ class TestPdfService(
                 PdfValidator.validerPdf(pdf, it.standard)
             }
         return standarder
-    }
-
-    private fun lesSøknadJson(): Map<String, Any> {
-        val jsonInputStream =
-            this::class.java.getResourceAsStream("/søknad.json")
-                ?: throw FileNotFoundException("Kan ikke lese søknad.json")
-
-        return try {
-            jsonInputStream.bufferedReader().use { reader ->
-                objectMapper.readValue(reader, Map::class.java) as Map<String, Any>
-            }
-        } catch (e: IOException) {
-            throw RuntimeException("Feil ved lesing av JSON-fil", e)
-        }
     }
 }
