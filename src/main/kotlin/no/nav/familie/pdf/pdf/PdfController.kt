0e6c1f8d2bc9264c18e8c0f1cb59cbdbb091bd35
Sandra Lekve <sandra.lekve@icloud.com>
2024-11-27 16:49:30 +0100

    legg til unprotected

--- src/main/kotlin/no/nav/familie/pdf/pdf/PdfController.kt
+++ src/main/kotlin/no/nav/familie/pdf/pdf/PdfController.kt
@@ -1,11 +1,13 @@
 package no.nav.familie.pdf.pdf
 
+import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@Unprotected
 @RequestMapping("api/pdf")
 class PdfController {
     private val pdfService = PdfService()
