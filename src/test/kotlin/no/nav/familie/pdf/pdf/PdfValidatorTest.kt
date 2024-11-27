2e4dbd8e9e860fee9d24d28fc934e18999cafadb
Fredrik Garseg Mørk <31205303+fredrikmork@users.noreply.github.com>
2024-11-27 12:08:40 +0100

    Bruker bare pdf i Pdf-valideringstesten (#13)

--- src/test/kotlin/no/nav/familie/pdf/pdf/PdfValidatorTest.kt
+++ src/test/kotlin/no/nav/familie/pdf/pdf/PdfValidatorTest.kt
@@ -3,7 +3,6 @@ package no.nav.familie.pdf.no.nav.familie.pdf.pdf
 import no.nav.familie.pdf.pdf.JsonLeser
 import no.nav.familie.pdf.pdf.PdfService
 import no.nav.familie.pdf.pdf.PdfValidator
-import no.nav.familie.pdf.pdf.TestPdfService
 import no.nav.familie.pdf.pdf.domain.PdfStandard
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeAll
@@ -19,8 +18,7 @@ class PdfValidatorTest {
     @BeforeAll
     fun setup() {
         val feltMap = JsonLeser.lesSøknadJson()
-        val pdfMedStandarder = TestPdfService(pdfService).opprettPdfMedStandarder(feltMap)
-        pdfBytes = pdfMedStandarder.pdf
+        pdfBytes = pdfService.opprettPdf(feltMap)
     }
 
     @ParameterizedTest
