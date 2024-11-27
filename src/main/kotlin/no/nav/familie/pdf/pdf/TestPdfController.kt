9bb9624694fe75e6a91634ff20e4e5450a4783b5
Sandra Lekve <sandra.lekve@icloud.com>
2024-11-27 15:16:25 +0100

    fiks bug

--- src/main/kotlin/no/nav/familie/pdf/pdf/TestPdfController.kt
+++ src/main/kotlin/no/nav/familie/pdf/pdf/TestPdfController.kt
@@ -1,6 +1,8 @@
 package no.nav.familie.pdf.pdf
 
 import no.nav.familie.pdf.pdf.domain.PdfMedStandarder
+import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -8,14 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@Unprotected
 @RequestMapping("api/test-pdf")
 class TestPdfController {
     private val pdfService = PdfService()
     private val testPdfService = TestPdfService(pdfService)
 
+    @CrossOrigin(origins = ["http://localhost:5173"])
     @GetMapping("/pdf-med-standarder")
     fun hentPdfFraResourceMedStandarder(): PdfMedStandarder = testPdfService.opprettTestPdfMedStandarder()
 
+    @CrossOrigin(origins = ["http://localhost:5173"])
     @PostMapping("/pdf-med-standarder")
     fun opprettPdfMedValidering(
         @RequestBody søknad: Map<String, Any>,
