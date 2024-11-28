5fd7a0ea4c6a059c212cf57022bf52048cdf7032
Sandra Lekve <sandra.lekve@icloud.com>
2024-11-28 10:48:52 +0100

    add crossOrigin

--- src/main/kotlin/no/nav/familie/pdf/pdf/TestPdfController.kt
+++ src/main/kotlin/no/nav/familie/pdf/pdf/TestPdfController.kt
@@ -1,6 +1,8 @@
 package no.nav.familie.pdf.pdf
 
 import no.nav.familie.pdf.pdf.domain.PdfMedStandarder
+import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -8,15 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@Unprotected
 @RequestMapping("api/test-pdf")
 class TestPdfController {
     private val pdfService = PdfService()
     private val testPdfService = TestPdfService(pdfService)
 
+    @CrossOrigin(origins = ["http://localhost:5173"])
     @GetMapping("/pdf-med-standarder")
     fun hentPdfFraResourceMedStandarder(): PdfMedStandarder = testPdfService.opprettTestPdfMedStandarder()
 
     @PostMapping("/pdf-med-standarder")
+    @CrossOrigin(origins = ["http://localhost:5173"])
     fun opprettPdfMedValidering(
         @RequestBody søknad: Map<String, Any>,
     ): PdfMedStandarder = testPdfService.opprettPdfMedStandarder(søknad)
