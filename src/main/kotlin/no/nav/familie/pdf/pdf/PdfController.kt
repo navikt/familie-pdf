279ab6dceef7e5d1d97a222ba83f9bc97b7eb373
Fredrik Garseg Mørk <31205303+fredrikmork@users.noreply.github.com>
2024-12-03 14:19:41 +0100

    La til protected med claims (#26)

--- src/main/kotlin/no/nav/familie/pdf/pdf/PdfController.kt
+++ src/main/kotlin/no/nav/familie/pdf/pdf/PdfController.kt
@@ -1,14 +1,16 @@
 package no.nav.familie.pdf.pdf
 
-import no.nav.security.token.support.core.api.Unprotected
+import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@Unprotected
 @RequestMapping("api/pdf")
+@ProtectedWithClaims(
+    issuer = "azuread",
+)
 class PdfController {
     private val pdfService = PdfService()
 
--- src/main/kotlin/no/nav/familie/pdf/pdf/TestPdfController.kt
+++ src/main/kotlin/no/nav/familie/pdf/pdf/TestPdfController.kt
@@ -1,7 +1,7 @@
 package no.nav.familie.pdf.pdf
 
 import no.nav.familie.pdf.pdf.domain.PdfMedStandarder
-import no.nav.security.token.support.core.api.Unprotected
+import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@Unprotected
 @RequestMapping("api/test-pdf")
+@ProtectedWithClaims(
+    issuer = "azuread",
+)
 class TestPdfController {
     private val pdfService = PdfService()
     private val testPdfService = TestPdfService(pdfService)
