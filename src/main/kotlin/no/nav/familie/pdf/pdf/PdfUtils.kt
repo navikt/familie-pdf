7c63fc13626cec46f00cfd019a9381766bbc6c16
Marcus <marcus.schroder@bekk.no>
2024-12-11 11:45:56 +0100

    legge til div rundt label og verdi. keeptogheter på diven

--- src/main/kotlin/no/nav/familie/pdf/pdf/PdfUtils.kt
+++ src/main/kotlin/no/nav/familie/pdf/pdf/PdfUtils.kt
@@ -195,15 +195,22 @@ object PdfUtils {
         verdiliste.filterIsInstance<Map<*, *>>().forEach { element ->
             val verdilisteBarn = element["verdiliste"] as? List<*>
             val marginVenstre = 15f * rekursjonsDybde
-            if (verdilisteBarn != null && verdilisteBarn.isNotEmpty()) {
-                seksjon.add(lagOverskriftH3(element["label"].toString()).apply { setMarginLeft(marginVenstre)
+            Div().apply {
                 isKeepTogether = true
-                })
-                håndterRekursivVerdiliste(verdilisteBarn, seksjon, rekursjonsDybde + 1)
-            } else if (element["verdi"] != null) {
-                seksjon.add(lagVerdiElement(element).apply { setMarginLeft(marginVenstre)
-                isKeepTogether = true
-                })
+                if (verdilisteBarn != null && verdilisteBarn.isNotEmpty()) {
+                    seksjon.add(
+                        lagOverskriftH3(element["label"].toString()).apply {
+                            setMarginLeft(marginVenstre)
+                        },
+                    )
+                    håndterRekursivVerdiliste(verdilisteBarn, seksjon, rekursjonsDybde + 1)
+                } else if (element["verdi"] != null) {
+                    seksjon.add(
+                        lagVerdiElement(element).apply {
+                            setMarginLeft(marginVenstre)
+                        },
+                    )
+                }
             }
         }
     }
