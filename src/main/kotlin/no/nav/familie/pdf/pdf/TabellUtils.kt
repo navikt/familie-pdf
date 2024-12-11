f9436de01e3887569e4c9032e3c96e5942396ee8
Marcus <marcus.schroder@bekk.no>
2024-12-11 12:07:36 +0100

    keeptogether på barnetabell

--- src/main/kotlin/no/nav/familie/pdf/pdf/TabellUtils.kt
+++ src/main/kotlin/no/nav/familie/pdf/pdf/TabellUtils.kt
@@ -20,6 +20,7 @@ object TabellUtils {
                 setMarginBottom(10f)
                 setMarginLeft(15f)
                 accessibilityProperties.role = StandardRoles.TABLE
+                isKeepTogether = true
             }
         val captionDiv =
             Div().apply {
