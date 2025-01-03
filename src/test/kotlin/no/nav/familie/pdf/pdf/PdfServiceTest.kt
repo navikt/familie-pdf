4738a94721543da5c0d6c72b9652519d0c8f244c
Sandra Lekve <sandra.lekve@icloud.com>
2025-01-03 12:05:03 +0100

    fiks tester

--- src/test/kotlin/no/nav/familie/pdf/pdf/PdfServiceTest.kt
+++ src/test/kotlin/no/nav/familie/pdf/pdf/PdfServiceTest.kt
@@ -16,6 +16,7 @@ import no.nav.familie.pdf.no.nav.familie.pdf.pdf.utils.lagToSiderInnholdsfortegn
 import no.nav.familie.pdf.pdf.PdfService
 import no.nav.familie.pdf.pdf.domain.FeltMap
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
@@ -47,7 +48,6 @@ class PdfServiceTest {
                 lagAdresseMedBareLinjeskift(),
                 lagMedTomAdresse(),
             )
-
         @JvmStatic
         fun flereArbeidsforhold(): Stream<FeltMap> =
             Stream.of(
