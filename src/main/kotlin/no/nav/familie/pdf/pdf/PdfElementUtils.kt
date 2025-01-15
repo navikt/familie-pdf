44e60c359662f6e33cf5e0a8c167d29bca163c3d
SandraLekve <78852953+SandraLekve@users.noreply.github.com>
2025-01-15 10:13:12 +0100

    Fjerne dobbel linjeavstand (#63)

--- src/main/kotlin/no/nav/familie/pdf/pdf/PdfElementUtils.kt
+++ src/main/kotlin/no/nav/familie/pdf/pdf/PdfElementUtils.kt
@@ -40,11 +40,7 @@ object PdfElementUtils {
                 )
             }
             add(Text("\n"))
-            if (element.label == "Adresse" && element.verdi != null) {
-                add(sjekkDobbelLinjeskift(element.verdi))
-            } else {
-                add(element.verdi)
-            }
+            add(element.verdi)
             setFontSize(12f)
             isKeepTogether = true
             accessibilityProperties.role = StandardRoles.P
@@ -85,18 +81,6 @@ object PdfElementUtils {
             isKeepTogether = true
             accessibilityProperties.role = StandardRoles.P
         }
-
-    private fun sjekkDobbelLinjeskift(tekst: String): String {
-        val bareLinjeskiftRegex = Regex("^\\n+$")
-        val dobbelLinjeskiftRegex = Regex("\\n{2,}")
-        val rensetVerdi =
-            if (tekst.isEmpty() || tekst.matches(bareLinjeskiftRegex)) {
-                "Ingen registrert adresse"
-            } else {
-                tekst.replace(dobbelLinjeskiftRegex, "\n")
-            }
-        return rensetVerdi
-    }
 
     fun lagTekstElement(
         tekst: String,
