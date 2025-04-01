79f1e9fcb72e046430920230dee9159b8adff27e
Robin Lund Sadun <47403589+RobinLundSadun@users.noreply.github.com>
2025-04-01 12:12:19 +0200

    Endepunkt som returnerer json (#104)

--- src/main/kotlin/no/nav/familie/pdf/pdf/PdfController.kt
+++ src/main/kotlin/no/nav/familie/pdf/pdf/PdfController.kt
@@ -3,6 +3,7 @@ package no.nav.familie.pdf.pdf
 import no.nav.familie.pdf.pdf.domain.FeltMap
 import no.nav.familie.pdf.pdf.språkKonfigurasjon.SpråkKontekst
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,11 +17,14 @@ import org.springframework.web.bind.annotation.RestController
 class PdfController {
     private val pdfService = PdfService()
 
+    data class PdfResponse(
+        val pdf: ByteArray,
+    )
+
     @PostMapping("/opprett-pdf")
     fun opprettPdf(
         @RequestBody søknad: FeltMap,
     ): ByteArray {
-
         try {
             SpråkKontekst.settSpråk(søknad.pdfConfig.språk)
             val returverdi = pdfService.opprettPdf(søknad)
@@ -28,6 +32,18 @@ class PdfController {
         } finally {
             SpråkKontekst.tilbakestillSpråk()
         }
+    }
 
+    @PostMapping("/opprett-pdf/som-json", produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun opprettPdfSomJson(
+        @RequestBody søknad: FeltMap,
+    ): PdfResponse {
+        try {
+            SpråkKontekst.settSpråk(søknad.pdfConfig.språk)
+            val returverdi = pdfService.opprettPdf(søknad)
+            return PdfResponse(returverdi)
+        } finally {
+            SpråkKontekst.tilbakestillSpråk()
+        }
     }
 }
