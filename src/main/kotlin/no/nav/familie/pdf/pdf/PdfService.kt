b6afe3f4a8bf28129a2c5c987ee453e3d59f5d03
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2025-04-10 13:09:17 +0200

    Fjerner ubrukt import

--- src/main/kotlin/no/nav/familie/pdf/pdf/PdfService.kt
+++ src/main/kotlin/no/nav/familie/pdf/pdf/PdfService.kt
@@ -1,7 +1,5 @@
 package no.nav.familie.pdf.pdf
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import com.itextpdf.io.source.ByteArrayOutputStream
 import no.nav.familie.pdf.pdf.PDFdokument.lagPdfADocument
 import no.nav.familie.pdf.pdf.PDFdokument.lagSøknadskvittering
