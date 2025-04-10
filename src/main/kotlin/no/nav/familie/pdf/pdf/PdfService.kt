ab301de3885b799a4198b2d12bbcba7f251ee6a8
Ole Christian Kvernberg <42737566+olekvernberg@users.noreply.github.com>
2025-04-10 12:43:24 +0200

    Fjerner tabs fra søknad (#106)
    Co-authored-by: RobinLundSadun <robin.lund.sadun@gmail.com>

--- src/main/kotlin/no/nav/familie/pdf/pdf/PdfService.kt
+++ src/main/kotlin/no/nav/familie/pdf/pdf/PdfService.kt
@@ -1,5 +1,7 @@
 package no.nav.familie.pdf.pdf
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.itextpdf.io.source.ByteArrayOutputStream
 import no.nav.familie.pdf.pdf.PDFdokument.lagPdfADocument
 import no.nav.familie.pdf.pdf.PDFdokument.lagSøknadskvittering
@@ -9,7 +11,10 @@ class PdfService {
     fun opprettPdf(feltMap: FeltMap): ByteArray {
         val byteArrayOutputStream = ByteArrayOutputStream()
         val pdfADokument = lagPdfADocument(byteArrayOutputStream)
-        lagSøknadskvittering(pdfADokument, feltMap)
+        val feltMapJson = jacksonObjectMapper().writeValueAsString(feltMap)
+        val feltMapJsonUtenTabs = feltMapJson.replace("\\t", "")
+        val feltMapUtenTabs = jacksonObjectMapper().readValue(feltMapJsonUtenTabs, FeltMap::class.java)
+        lagSøknadskvittering(pdfADokument, feltMapUtenTabs)
 
         return byteArrayOutputStream.toByteArray()
     }
